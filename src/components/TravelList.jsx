aa5e3723eea6e4e34a0fbd7cc2971826d287db8c
Deniz Cilingir <deniz.cilingir@gmx.de>
2024-01-30 17:07:53 +0100

    Solved Lab

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,35 +1,21 @@
-import { useState } from "react";
-import travelPlansData from "../assets/travel-plans.json";
+import { useState } from 'react'
+import TravelPlanCard from "/src/components/TravelPlanCard";
+import travelPlansData from "/src/assets/travel-plans.json";
 
 const TravelList = () => {
-  // Using useState to store the travel plans data in the component state
-  const [travelPlans, setTravelPlans] = useState(travelPlansData);
+  const [travelPlan, setTravelPlan] = useState(travelPlansData)
+
+  const deleteItemHandler = (id) => {
+    setTravelPlan(travelPlan.filter(travel => travel.id !== id))
+  }
 
   return (
-    <div className="travelcard">
-      <h2>Travel Plans</h2>
-      {travelPlans.map((plan) => (
-        <div className="destination" key={plan.id}>
-          <h2>
-            {plan.destination} ({plan.days} Days)
-          </h2>
-          <img
-            src={plan.image}
-            alt={plan.destination}
-            style={{ maxWidth: "300px" }}
-          />
-          <p>
-            <italic>{plan.description}</italic>
-          </p>
-          <p>
-            <strong>Price:</strong> {plan.totalCost} €
-          </p>
-          <ul></ul>
-          <hr />
-        </div>
+    <div>
+      {travelPlan.map(travel => (
+        <TravelPlanCard key={travel.id} plan={travel} onClickDelete={deleteItemHandler} />
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default TravelList;
+export default TravelList
