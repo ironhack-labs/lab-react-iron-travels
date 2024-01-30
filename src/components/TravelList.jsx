7a84f84d905e0036b6f5dd30d9b50c7f51dee0df
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-30 21:56:38 +0100

    lab solved without bonus

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,21 +1,50 @@
 import { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
+import "./TravelList.css";
+
+function dealOrNoDeal(totalCost) {
+  if (totalCost <= 350) {
+    return <span className="green">Great Deal</span>;
+  } else if (totalCost >= 1500) {
+    return <span className="blue">Premium</span>;
+  } else {
+    return null;
+  }
+}
 
 function TravelList() {
   const [data, setData] = useState(travelPlansData);
 
+  const handleDelete = (id) => {
+    const updatedTravelPlans = data.filter((item) => item.id !== id);
+    setData(updatedTravelPlans);
+  };
   return (
-    <div>
-      <h2>Travel list</h2>
+    <div className="main">
       {data.map((item) => {
         return (
-          <div key={item.id}>
-            <img src={item.image} alt="pic" style={{ width: 300 }} />
-            <h3>
-              {item.destination} ({item.days} days)
-            </h3>
-            <i>{item.description}</i>
-            <p>Price: {item.totalCost} €</p>
+          <div key={item.id} className="trip-div">
+            <img src={item.image} alt="pic" className="trip-pic" />
+            <div className="text-div">
+              <h3>
+                {item.destination} ({item.days} days)
+              </h3>
+              <i>{item.description}</i>
+              <p>Price: {item.totalCost} € </p>
+              <div className="labels">
+                <span>{dealOrNoDeal(item.totalCost)}</span>
+                {item.allInclusive && (
+                  <span className="inclusive">All inclusive </span>
+                )}
+                </div>
+                <button
+                  onClick={() => handleDelete(item.id)}
+                  className="delete-button"
+                >
+                  Delete
+                </button>
+             
+            </div>
           </div>
         );
       })}
