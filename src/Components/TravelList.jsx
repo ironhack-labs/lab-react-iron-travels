3caabc59d70ea8ab31a7a878025bddab60ba65d9
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-30 21:12:23 +0100

    add card comp

--- src/Components/TravelList.jsx
+++ src/Components/TravelList.jsx
@@ -1,48 +1,17 @@
 import React from 'react'
-import Label from './Label'
+import Card from './Card'
 
-const TravelList = ({ image, destination, days, description, totalCost }) => {
+const TravelList = ({ travel, onHandleDelete }) => {
   return (
-    <div 
-        style={{
-            border: '1px solid black',
-            width: '60%',
-            paddingTop: '20px',
-            paddingBottom: '20px',
-            paddingLeft: '30px',
-            paddingRight: '30px',
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'row',
-        }}
-    >
-        <div>
-            <img src={image} style={{ width: '20em' }} alt="travel pict" />
-        </div>
-        <div
-        style={{
-            textAlign: 'left',
-            marginLeft: '20px',
-        }}
-        >
-            <h3>{ destination } ( { days } days )</h3>
-            <p>{description}</p>
-            <p>Price: { totalCost } </p>
-            {
-                totalCost > 1500 && (
-                    <Label text="Premium" backgroundColor="#6fa8dc" /> 
-                ) 
-            }
-
-            {
-                totalCost < 350 && (
-
-                 <Label text="Great Deal" backgroundColor="#93c47d" /> 
-                )
-
-            }
-        </div>
-    </div>
+    <Card
+      id={travel.id}
+      image={travel.image}
+      destination={travel.destination}
+      days={travel.days}
+      description={travel.description}
+      totalCost={travel.totalCost}
+      onHandleDelete={onHandleDelete}
+     />
   )
 }
 
