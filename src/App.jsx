d2fcd4eede1bca29b2aeabb5935665428005ac17
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-30 16:42:39 +0100

    add components folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
+      <div>        
         <img src={logo} className="logo" alt="App logo" />
       </div>
       <h1 className="text-iron">Iron Travels</h1>
       <h3 className="text-iron">Tailored Travel Plans for Ironhackers</h3>
+
 
       {/* RENDER YOUR LIST COMPONENT HERE */}
       
