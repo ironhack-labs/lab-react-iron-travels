7747256cd893802f5ab7a45485cc307ad9af8277
Alastair Longmuir <alastairlinwood@gmail.com>
2024-02-06 00:18:53 +0100

    finished iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import logo from "./assets/logo.png";
 import "./App.css";
 import TravelList from './components/TravelList';
-import travelPlansData from '/src/assets/travel-plans.json';
+
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <h1 className="text-iron">Iron Travels</h1>
       <h3 className="text-iron">Tailored Travel Plans for Ironhackers</h3>
 
-      {travelPlansData.map(destination => <TravelList trip={destination} key={destination.id} />)}
+      <TravelList />
       
     </>
   );
--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,35 +1,27 @@
-import {useState} from 'react';
+import travelPlansData from '/src/assets/travel-plans.json';
 
 function TravelList (prop) {
-
-    const [premium, setPremium] = useState(false);
-    const [allInc, setAllInc] = useState(false);
-    const [greatDeal, setGreatDeal] = useState(false);
-
-    if (prop.trip.totalCost >= 1500) setPremium(true);
-    if (prop.trip.totalCost <= 350) setAllInc(true);
-    if (prop.trip.allInclusive) setAllInc(true);
 
 
     return(
 
-        <div className="TravelList">
-            <img src={prop.trip.image} />
-            <div className="info">
-                <h3>{prop.trip.destination} &#40;{prop.trip.days} Days&#41;</h3>
-                <p><em>{prop.trip.description}</em></p>
-                <p><b>Price:</b> {prop.trip.totalCost} €</p>
+        travelPlansData.map(plan => (
 
-                {premium ? <button>All-Inclusive</button>:<p>no</p>}
-                {greatDeal ? <button>Great Deal</button>:<p>no</p>}
-                {allInc ? <button>All-Inclusive</button>:<p>no</p>}
+            <div className="TravelList" key={plan.id}>
+                <img src={plan.image} />
+                <div className="info">
+                    <h3>{plan.destination} &#40;{plan.days} Days&#41;</h3>
+                    <p><em>{plan.description}</em></p>
+                    <p><b>Price:</b> {plan.totalCost} €</p>
 
+                    {plan.totalCost <= 350 && <button className="green">Great Deal</button>}
+                    {plan.totalCost >= 1500 && <button className="blue">Premium</button>}
+                    {plan.allInclusive && <button className="blue">All-Inclusive</button>}
+
+                </div>   
             </div>
 
-
-
-            
-        </div>
+        ))
     )
 }
 
