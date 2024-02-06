a6def4ddc1b458c16332f099a94d7e2fc939769a
Senay Asikoglu <senayasikoglu@gmail.com>
2024-02-06 23:23:05 +0100

    lab-react-iron-travels done

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,28 +1,40 @@
 import travelPlansData from "../assets/travel-plans.json";
+import { useState } from "react";
 
-function TravelList(){
+function TravelList() {
+  const [travelPlansToDisplay, settravelPlansToDisplay] =
+    useState(travelPlansData);
 
-    const [travelPlansToDisplay, settravelPlansToDisplay] = useState(travelPlansData);
+  const deleteTravel = (id) => {
+    const updatedData = travelPlansToDisplay.filter((elm) => elm.id !== id);
+    settravelPlansToDisplay(updatedData);
+  };
 
+  return (
+    <section className="TravelList">
+      {travelPlansToDisplay.map((travelPlanDetails) => {
+        return (
+          <div key={travelPlanDetails.id} className="travel plans">
+            <img src={travelPlanDetails.image} alt="" />
+            <h2>{travelPlanDetails.destination}</h2>
 
-    
-    return(
-        <section className="TravelList">
+            <p> {travelPlanDetails.description}</p>
+            <p>
+              <strong>Price: </strong>
+              {travelPlanDetails.totalCost} €
+            </p>
 
-            {travelPlansToDisplay.map( (travelPlanDetails) => {
-                return(
-                    <div key={travelPlanDetails.id} className="travel plans">
-                        <h2>{travelPlanDetails.destination}</h2>
-
-                        <p> {travelPlanDetails.description}</p>
-                        <p>Price : {travelPlanDetails.totalCost}</p>
-                       
-                    </div>
-                );
-            })}
-
-        </section>
-    );
+            <div>
+              {travelPlanDetails.totalCost < 350 ? "Great Deal" : "Premium"}
+            </div>
+            <button onClick={() => deleteTravel(travelPlanDetails.id)}>
+              Delete
+            </button>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 
 export default TravelList;
