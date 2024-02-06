baa3d6a4c60cc80e876dcb30d1f68b84f244c125
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-02-06 17:13:10 +0100

    First iterations done. finish later

---
+++ src/Components/TravelList/TravelList.jsx
@@ -0,0 +1,53 @@
+import "./TravelList.css";
+
+
+import travelPlansData from "./../../assets/travel-plans.json";
+
+import { useState } from "react";
+
+
+function TravelList () {
+
+    const [travelIdeas, setTravelIdeas]  = useState(travelPlansData);
+    // Fetch the data 
+
+    const deleteButton = (travelPlan) => {
+        const newList = travelIdeas.filter((travelObj) => {
+            return  travelObj.id !== travelPlan;
+        });
+        
+        setTravelIdeas(newList);
+    } 
+
+    return (
+        <div className="travel-container">
+          <ul>
+            {travelIdeas.map((travelPlan) => (
+              <li key={travelPlan.id}>
+                <img src={travelPlan.image} alt={`Image of ${travelPlan.destination}`} />
+                <h3>{travelPlan.destination}, {travelPlan.days}</h3>
+                <p>{travelPlan.description}</p>
+                <span>Price: {travelPlan.totalCost} $</span>
+                
+                {travelPlan.totalCost <= 350 && <span className="green label">Great Deal</span>}
+               
+                {travelPlan.totalCost > 350 && <span className="blue label">Premium</span>}
+               
+                {travelPlan.allInclusive  === true && <span className="blue label">All Inclusive</span>}
+
+                <button onClick = {() =>  {deleteButton(travelPlan.id)}}>Delete</button>
+
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+}
+
+
+
+
+
+
+
+export default TravelList;
