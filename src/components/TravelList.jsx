b8c3d3c7e01d4f9580cd516e0b8a9b3fc8955691
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-02-06 22:14:28 +0100

    lab iron travel

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
 
-// let button 
-// if (travelPlansData.totalCost < 350) {
-//     button = <button>test</button>
-// }
+
 
 function TravelList () {
+
+    const [travelsToDisplay, setTravelsToDisplay] = useState(travelPlansData)
+
+    function deleteTravels (travelId) {
+        const newTravels = travelsToDisplay.filter((travelObj) => {
+            return travelObj.id !== travelId; 
+        })
+        setTravelsToDisplay(newTravels);
+    }
+
     return (
         <div className="boxes">
-            {travelPlansData.map((element) => {
+            {travelsToDisplay.map((element) => {
                return (
                 <div className="destination">
                     <img src={element.image} />
@@ -17,10 +24,14 @@ function TravelList () {
                         <h3>{element.destination} ({element.days} Days)</h3>
                         <p><i>{element.description}</i></p>
                         <p><strong>Price:</strong> {element.totalCost} €</p>
-                        {element.totalCost < 350 && <button>test</button>}
-                        
-                        
+                        {element.totalCost < 350 && <button disabled className="greate-deal">Greate Deal</button>}
+                        {element.totalCost > 1500 && <button disabled className="premium">Premium</button>}
+                        {element.allInclusive && <button disabled className="premium">All-Inclusive</button>}
+                        <br/>
+                        <button className="delete" onClick={()=>{deleteTravels(element.id)}}>Delete</button>
+                        <button className="favorite">heart</button>
                     </div>
+                    
                 </div>
                 
                ) 
