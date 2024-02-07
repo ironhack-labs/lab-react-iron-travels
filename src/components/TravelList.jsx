ba00b2cd1dfd0c89d251ea761091290c8c0d4b96
Daniel_Costa <dan.costaa.12@gmail.com>
2024-02-07 17:09:59 +0000

    solved lab - without bonus and styles

---
+++ src/components/TravelList.jsx
@@ -0,0 +1,60 @@
+import {useState} from 'react'
+import travelplans from '../assets/travel-plans.json'
+
+function TravelList(){
+   
+    const [plans, setPlans] = useState(travelplans);
+    // create a new variable with a new array to work on
+    const newPlans = [...plans];
+    // loop in the array to define plan into an Index
+    for (let i=0;i<travelplans.length;i++){
+       const plan = newPlans[i]
+    }
+    // delete button - create a new variable with a new array without the index filtered.
+    const deletePlan = (planId) => {
+        const updatedPlans = plans.filter((plan) => plan.id !== planId);
+        setPlans(updatedPlans);
+    };
+
+    
+    return(
+        <div>
+        <div>
+            {newPlans.map((plan)=>{
+                return (
+                    <div key={plan.id}>
+                        <img src={plan.image} />
+                        <h3>{plan.destination}</h3>
+                        <p>{plan.description}</p>
+                        <p>Price: {plan.totalCost} €</p>
+                        <div>
+                            {plan.totalCost <= 350 ?  <p><strong>Great Deal</strong></p> : null}
+                            {plan.totalCost >= 1500 ?  <p><strong>Premium</strong></p> : null}
+                            {plan.allInclusive && <p><strong>All Inclusive</strong></p>}
+                        </div>
+                        <button onClick={() => deletePlan(plan.id)}>Delete Plan</button>
+                    </div>   
+                    )
+                })}
+        </div>
+    </div>
+    )
+
+/* function deleteBtn(){
+    const [showPlan, setShowPlans] = useState(true);
+ 
+    const deletePlan = planId => {
+    const filteredPlan = plans.filter(plan => {
+      return plan._id !== planId;
+    });
+    setPlans(filteredMovies);
+  };
+ 
+  const toggleShowPlans = () => {
+    setShowPlans(!showPlans);
+  };
+  <button onClick={toggleShowPlans}>{showPlans ? 'Hide' : 'Show'}</button>
+} */
+}
+
+export default TravelList;
