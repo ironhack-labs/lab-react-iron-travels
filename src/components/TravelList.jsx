68692d9a617d6fbf0b73663d8124d499ff945673
Shannon <shannonblas@pucpr.edu>
2024-02-28 20:38:20 -0500

    almost done

---
+++ src/components/TravelList.jsx
@@ -0,0 +1,70 @@
+import travelPlansData from "../assets/travel-plans.json";
+import { useState } from "react";
+
+const TravelList = () => {
+  const [travelList, setTravelList] = useState(travelPlansData);
+
+
+  const deleteTravelCard = (cardId) => {
+    const filteredTravelCards = travelData.filter((travelCard) => {
+      return travelCard.id !== cardId;
+    });
+    setTravelList(filteredTravelCards);
+  };
+
+
+
+  return (
+    <ul>
+      {travelList.map((item) => {
+        return (
+          <li key={item.id}>
+            <div className="flex-container">
+              <div className="image-div">
+                <img
+                  className="image"
+                  src={item.image}
+                  alt={item.destination}
+                />
+              </div>
+
+              <div className="content-div">
+                <h3>
+                  {item.destination} ({item.days} Days)
+                </h3>
+
+                <p>
+                  <i> {item.description} </i>
+                </p>
+
+                
+                <p>
+                  <strong> Price </strong> {item.totalCost} <strong>€</strong>{" "}
+                </p>
+
+                {/* Premium Label  */}
+                    {item.totalCost > 1500 && (  <div className="premium"> Premium </div> )}
+                {/* Great Deal Label */}
+                    {item.totalCost < 350 && (  <div className="great-deal"> Great Deal </div>  )}
+                {/* All Inclusive  */}
+                    {item.allInclusive && (<div className="all-inc"> All-Inclusive </div>  )}
+              
+
+
+                {/* Delete button  */}
+                <button className="delete-button" onClick={() => deleteTravelCard(item.id)}> Delete </button>
+
+
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+
+
+
+  );
+};
+
+export default TravelList;
