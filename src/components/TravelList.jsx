afa3e88b8f49dff91df8925790a20ddd0cc0bfd7
Shannon <shannonblas@pucpr.edu>
2024-02-28 20:58:36 -0500

    - delete button added

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -4,15 +4,12 @@ import { useState } from "react";
 const TravelList = () => {
   const [travelList, setTravelList] = useState(travelPlansData);
 
-
   const deleteTravelCard = (cardId) => {
-    const filteredTravelCards = travelData.filter((travelCard) => {
-      return travelCard.id !== cardId;
-    });
+    const filteredTravelCards = travelList.filter(
+      (travelCard) => travelCard.id !== cardId
+    );
     setTravelList(filteredTravelCards);
   };
-
-
 
   return (
     <ul>
@@ -37,33 +34,36 @@ const TravelList = () => {
                   <i> {item.description} </i>
                 </p>
 
-                
                 <p>
                   <strong> Price </strong> {item.totalCost} <strong>€</strong>{" "}
                 </p>
 
                 {/* Premium Label  */}
-                    {item.totalCost > 1500 && (  <div className="premium"> Premium </div> )}
+                {item.totalCost > 1500 && (
+                  <div className="premium"> Premium </div>
+                )}
                 {/* Great Deal Label */}
-                    {item.totalCost < 350 && (  <div className="great-deal"> Great Deal </div>  )}
+                {item.totalCost < 350 && (
+                  <div className="great-deal"> Great Deal </div>
+                )}
                 {/* All Inclusive  */}
-                    {item.allInclusive && (<div className="all-inc"> All-Inclusive </div>  )}
-              
-
+                {item.allInclusive && (
+                  <div className="all-inc"> All-Inclusive </div>
+                )}
 
                 {/* Delete button  */}
-                <button className="delete-button" onClick={() => deleteTravelCard(item.id)}> Delete </button>
-
-
+                <button
+                  className="delete-button"
+                  onClick={() => deleteTravelCard(item.id)}
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </li>
         );
       })}
     </ul>
-
-
-
   );
 };
 
