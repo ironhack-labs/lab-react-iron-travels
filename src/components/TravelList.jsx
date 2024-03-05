dccc69936ef11162baaf236a8dbd925fa480d543
Memoryman2022 <rlacannon@gmail.com>
2024-03-05 22:19:05 +0100

    solved lab

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -5,6 +5,11 @@ import travelPlansData from "../assets/travel-plans.json";
 export const TravelList = () => {
   let [travelArray, setTravelArray] = useState(travelPlansData);
 
+  const deleteById = (id) => {
+    setTravelArray((city) => {
+      return city.filter((city) => city.id !== id);
+    });
+  };
   return (
     <div className="tripContainer">
       <ul>
@@ -16,11 +21,30 @@ export const TravelList = () => {
                 {city.destination}, ({city.days} Days)
               </div>
               <div className="italics">{city.description}</div>
+              <div className="tags">
+                <div className={tagHandler(city.totalCost)}>
+                  {tagHandler(city.totalCost) == "deal"
+                    ? "Great Deal"
+                    : "Premium"}{" "}
+                </div>
+                <div className="inclusive">
+                  {city.allInclusive ? "All inclusive" : ""}
+                </div>
+              </div>
               <div className="bold">Price: {city.totalCost} €</div>
             </div>
+            <button onClick={() => deleteById(city.id)}>Delete</button>
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
+function tagHandler(cost) {
+  if (cost <= 350) {
+    return "deal";
+  } else if (cost > 1500) {
+    return "premium";
+  }
+}
